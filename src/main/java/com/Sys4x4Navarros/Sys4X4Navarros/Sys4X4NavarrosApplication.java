de7e0a535722e2a5232b2b82eff9d47c6a4cd5ee
danielvilla04 <danielvillalobos0408@gmail.com>
2024-02-22 07:24:01 -0600

    Uploading first project files

---
+++ src/main/java/com/Sys4x4Navarros/Sys4X4Navarros/Sys4X4NavarrosApplication.java
@@ -0,0 +1,16 @@
+package com.Sys4x4Navarros.Sys4X4Navarros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@SpringBootApplication
+public class Sys4X4NavarrosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sys4X4NavarrosApplication.class, args);
+    }
+
+    
+}
---
+++ src/test/java/com/Sys4x4Navarros/Sys4X4Navarros/Sys4X4NavarrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sys4x4Navarros.Sys4X4Navarros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sys4X4NavarrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
